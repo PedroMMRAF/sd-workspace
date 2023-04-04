3ee8b21e4f8e480f7b3bc904dd5bd30106c49c61
PedroMMRAF <pmmr.fernandes@campus.fct.unl.pt>
2023-04-04 11:01:57 +0100

    mudancas

---
+++ Pedro/aula5/src/aula5/servers/rest/RestResource.java
@@ -0,0 +1,34 @@
+package aula5.servers.rest;
+
+import aula5.api.java.Result;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response.Status;
+
+public class RestResource {
+	/**
+	 * Given a Result<T>, either returns the value, or throws the JAX-WS Exception
+	 * matching the error code...
+	 */
+	protected <T> T fromJavaResult(Result<T> result) {
+		if (result.isOK())
+			return result.value();
+		else
+			throw new WebApplicationException(statusCodeFrom(result));
+	}
+
+	/**
+	 * Translates a Result<T> to a HTTP Status code
+	 */
+	private static Status statusCodeFrom(Result<?> result) {
+		return switch (result.error()) {
+			case CONFLICT -> Status.CONFLICT;
+			case NOT_FOUND -> Status.NOT_FOUND;
+			case FORBIDDEN -> Status.FORBIDDEN;
+			case TIMEOUT, BAD_REQUEST -> Status.BAD_REQUEST;
+			case NOT_IMPLEMENTED -> Status.NOT_IMPLEMENTED;
+			case INTERNAL_ERROR -> Status.INTERNAL_SERVER_ERROR;
+			case OK -> result.value() == null ? Status.NO_CONTENT : Status.OK;
+			default -> Status.INTERNAL_SERVER_ERROR;
+		};
+	}
+}
